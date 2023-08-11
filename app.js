3d54ef39c22ab60de584d1d32dda27e398239f8a
Sahil <garg07825@gmail.com>
2023-08-11 13:19:25 +0530

    Server started

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Task Manager App')
+import express from "express";
+const Port = 3000;
+const app = express();
+
+// routes testing server
+app.get('/', (req, res) => {
+    res.send("task manager is working properly")
+})
+
+app.listen(Port, (err) => {
+    if(err) return console.log(err.message);
+    console.log("server is working properly on " + Port);
+})
